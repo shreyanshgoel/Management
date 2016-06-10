5f2d79285e8f03ae492ce2ff4f154c4edecbb8ae
shreyansh goel <shreyanshgoel9@gmail.com>
2016-06-11 00:17:06 +0530

    Project initiated

---
+++ application/controllers/home.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * The Default Example Controller Class
+ *
+ * @author Faizan Ayubi, Hemant Mann
+ */
+use Shared\Controller as Controller;
+
+class Home extends Controller {
+
+    public function index() {
+    	$layoutView = $this->getLayoutView();
+    	$layoutView->set("seo", Framework\Registry::get("seo"));
+    }
+
+}
